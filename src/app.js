dd704eb6316caa7fb53552723f0e9b5d2aeeaecf
sandrabrooke <sandrabrooke@users.noreply.github.com>
2020-10-13 11:46:17 -0500

    removed curly braces around city name

--- src/app.js
+++ src/app.js
@@ -1,4 +1,3 @@
 let apiKey = "2ba862d4ef516628d352b5c22633316e";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q={NewYork}&appid=${apiKey}&units=metric`;
-
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=NewYork&appid=${apiKey}&units=metric`;
 console.log(apiUrl);
